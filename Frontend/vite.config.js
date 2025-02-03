aef42dac32a37f4f03f372ac825a60a9c0236645
Pankajtrivedi-2029 <pankajtrivedi2981@gmail.com>
2025-02-03 10:47:49 +0530

    vercel file's issue fixed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// export default {
+//   build: {
+//     sourcemap: false, // Disable sourcemaps
+//     minify: 'esbuild', // Use esbuild for minification (it doesn't require eval)
+//     target: 'esnext', // Ensure compatibility with modern browsers
+//     esbuild: {
+//       legalComments: 'none', // Remove any comments in the minified code
+//     },
+//   },
+// };
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: false, // Disable sourcemaps
+    minify: 'esbuild', // Use esbuild for minification (it doesn't require eval)
+    target: 'esnext', // Ensure compatibility with modern browsers
+    esbuild: {
+      legalComments: 'none', // Remove any comments in the minified code
+    },
+  },
+});
